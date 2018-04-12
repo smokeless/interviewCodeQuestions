896427dedc0d7f8caf460cff9e099d8e81cc0016
Ross Russell <ross.russell.ca@gmail.com>
2018-04-12 13:39:08 -0700

    start

---
+++ challenges.py
@@ -0,0 +1,77 @@
+#challenges from http://nbviewer.jupyter.org/github/donnemartin/interactive-coding-challenges/tree/master/
+
+from nose.tools import assert_equal
+
+class UniqueChars(object):
+
+    def has_unique_chars(self, string):
+        if string == None:
+            return False
+
+        if len(set(string)) < len(string):
+            return False
+        else:
+            return True
+
+
+class TestUniqueChars(object):
+
+    def test_unique_chars(self, func):
+        assert_equal(func(None), False)
+        assert_equal(func(''), True)
+        assert_equal(func('foo'), False)
+        assert_equal(func('bar'), True)
+        print('Success: test_unique_chars')
+
+
+def hasUniqueTest():
+    test = TestUniqueChars()
+    unique_chars = UniqueChars()
+    test.test_unique_chars(unique_chars.has_unique_chars)
+    try:
+        unique_chars_set = UniqueCharsSet()
+        test.test_unique_chars(unique_chars_set.has_unique_chars)
+        unique_chars_in_place = UniqueCharsInPlace()
+        test.test_unique_chars(unique_chars_in_place.has_unique_chars)
+    except NameError:
+        # Alternate solutions are only defined
+        # in the solutions file
+        pass
+
+
+class Permutations(object):
+
+    def is_permutation(self, str1, str2):
+        if str1 is None or str2 is None:
+            return False
+
+        return sorted(str1) == sorted(str2)
+
+
+class TestPermutation(object):
+
+    def test_permutation(self, func):
+        assert_equal(func(None, 'foo'), False)
+        assert_equal(func('', 'foo'), False)
+        assert_equal(func('Nib', 'bin'), False)
+        assert_equal(func('act', 'cat'), True)
+        assert_equal(func('a ct', 'ca t'), True)
+        print('Success: test_permutation')
+
+
+def testperms():
+    test = TestPermutation()
+    permutations = Permutations()
+    test.test_permutation(permutations.is_permutation)
+    try:
+        permutations_alt = PermutationsAlt()
+        test.test_permutation(permutations_alt.is_permutation)
+    except NameError:
+        # Alternate solutions are only defined
+        # in the solutions file
+        pass
+
+
+if __name__ == '__main__':
+    testperms()
+    pass
